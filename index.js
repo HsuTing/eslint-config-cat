977b14f13b7cb5a09aa4e5f5c598d9731bec15b3
HsuTing <hsuting0106@gmail.com>
2018-01-24 23:02:37 +0800

    feat(index.js): new rules

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ module.exports = {
   rules: {
     'max-len': 0,
     'quote-props': ['error', 'as-needed'],
-    'no-alert': 'off',
-    'no-console': 'off',
     'object-curly-spacing': [1, 'never'],
     'keyword-spacing': [2, {overrides: {
       if: {after: false},
@@ -17,7 +15,6 @@ module.exports = {
     'comma-dangle': ['error', 'never'],
     'arrow-parens': ['error', 'as-needed'],
     curly: ['error', 'multi-or-nest'],
-    'require-jsdoc': 'off',
     'no-invalid-this': 'off'
   }
 };
