4c9b352bcecc4d6d754c6c186c62311dce7aa532
HsuTing <hsuting0106@gmail.com>
2018-04-23 00:13:28 +0800

    refactor(index): change to default rules

--- index.js
+++ index.js
@@ -10,50 +10,28 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'fbjs/strict',
-    'google'
+    'google',
   ],
   parser: 'babel-eslint',
   env: {
     jest: true,
     node: true,
-    browser: true
+    browser: true,
   },
   plugins: [
     'import',
-    'flowtype'
+    'flowtype',
   ],
   settings: {
     'import/resolver': {
-      'babel-module': alias
+      'babel-module': alias,
     },
     flowtype: {
-      onlyFilesWithFlowAnnotation: true
-    }
+      onlyFilesWithFlowAnnotation: true,
+    },
   },
   rules: {
     indent: ['error', 2],
-    'max-len': 0,
     'quote-props': ['error', 'as-needed'],
-    'object-curly-spacing': [1, 'never'],
-    'keyword-spacing': [2, {overrides: {
-      if: {after: false},
-      for: {after: false},
-      while: {after: false},
-      switch: {after: false},
-      catch: {after: false}
-    }}],
-    'comma-dangle': ['error', 'never'],
-    'arrow-parens': ['error', 'as-needed'],
-    curly: ['error', 'multi-or-nest'],
-    'no-invalid-this': 'off',
-    'require-jsdoc': ['error', {
-      require: {
-        FunctionDeclaration: true,
-        MethodDefinition: true,
-        ClassDeclaration: true,
-        ArrowFunctionExpression: true,
-        FunctionExpression: true
-      }
-    }]
-  }
+  },
 };
